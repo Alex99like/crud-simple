550680cc1d2856cd1cc2e3480daf844a34de8644
Aleksander Aleksievich <aleksievisa@gmail.com>
2022-12-17 06:45:24 +0300

    init: init project

---
+++ webpack.config.ts
@@ -0,0 +1,28 @@
+import * as path from 'path';
+import webpack from 'webpack';
+
+const config: webpack.Configuration = {
+  mode: 'development',
+  entry: './src/main.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devtool: 'inline-source-map',
+  plugins: [],
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+};
+
+export default config;
