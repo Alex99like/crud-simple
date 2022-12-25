f827ff49e2a7736e23e5ce8101c9b3ce439d8738
Alexander <aleksievisa@gmail.com>
2022-12-25 05:47:49 +0300

    fix: fix mthod create

--- src/Server.ts
+++ src/Server.ts
@@ -10,11 +10,12 @@ export class Server {
   db: IUser[];
   server: http.Server<typeof http.IncomingMessage, typeof http.ServerResponse>;
   emitter: EventEmitter;
+  userService: UserService;
 
   constructor() {
     this.db = [{
       id: "f0222d8c-2ce1-4c36-a41e-3593d8c7c621",
-      username: "Alex",
+      username: "Alex",  
       hobbies: [
         "game"
       ],
@@ -31,119 +32,15 @@ export class Server {
   ]
     this.emitter = new EventEmitter()
     this.server = this.createServer()
+    this.userService = new UserService()
     this.setEndpoints()
   }
 
-  getUser(req: ReqType, res: ResType, parameter: string) {
-    if (parameter) {
-      const [user] = this.db.filter(el => el.id === parameter)
-      if (user) {
-        if (validate(user.id)) {
-          res.send(HttpCode.Success, user)
-        } else {
-          res.send(HttpCode.BadReq, { message: `this Id: ${user.id} is not valid` })
-        }
-      } else {
-        res.send(HttpCode.NotFound, { message: `there is no user with the same - ${parameter}` })
-      }
-    } else {
-      res.send(HttpCode.Success, this.db)
-    }
-  }
-
-  createUser(req: ReqType, res: ResType, parameter: string) {
-    if (!parameter) {
-      res.send(HttpCode.NotFound, { message: 'Not Found' })
-      return
-    } else {
-      let body = ''
-      req.on('data', (chunk) => {
-        body += chunk
-      })
-
-      req.on('end', () => {
-        try {
-          const user = JSON.parse(body)
-          const errors = checkCreate(user)
-
-          if (errors.length === 0) {
-            const id = v4()
-            this.db.push({ id, username: user.username, age: user.age, hobbies: user.hobbies })
-            res.send(HttpCode.Created, { id, ...user })
-          } else {
-            res.send(HttpCode.BadReq, { errors })
-          }
-        } catch(e) {
-          res.send(HttpCode.ErrorServer, { message: `failed to create user` })
-        }
-      })
-    }
-  }
-
-  updateUser(req: ReqType, res: ResType, parameter: string) {
-    if (!parameter) {
-      res.send(HttpCode.NotFound, { message: `Not Found: id not specified` })
-      return
-    } else if (!validate(parameter)) {
-      res.send(HttpCode.NotFound, { message: `this Id: ${parameter} is not valid` })
-      return
-    }
-
-    let body = ''
-    req.on('data', (chunk) => {
-      body += chunk
-    })
-    req.on('end', () => {
-      try {
-        const user = JSON.parse(body)
-        const errors = checkUpdate(user)
-
-        if (errors.length === 0) {
-          let resData: IUser | undefined = undefined
-          this.db = this.db.map((el) => {
-            if (el.id === parameter) {
-              const userItem = {
-                id: user.id ? user.id : el.id,
-                username: user.username ? user.username : el.username,
-                age: user.age ? user.age : el.age,
-                hobbies: user.hobbies ? user.hobbies : el.hobbies,
-              }
-              resData = userItem
-              return userItem
-            } else {
-              return el
-            }
-          })
-          res.send(HttpCode.Success, resData)
-
-        } else {
-          res.send(HttpCode.BadReq, { errors })
-        }
-      } catch(e) {
-        res.send(HttpCode.ErrorServer, { message: `failed to update user` })
-      }      
-    })
-  }
-
-  removeUser(req: ReqType, res: ResType, parameter: string) {
-    if (!parameter) {
-      res.send(HttpCode.BadReq, { message: `id not specified` })
-      return
-    }
-    const [user] = this.db.filter(el => el.id === parameter)
-    if (!user) {
-      res.send(HttpCode.BadReq, { message: `there is no user with this id` })
-      return
-    }
-    this.db = this.db.filter(el => el.id !== parameter)
-    res.send(HttpCode.Delete , user)
-  }
-
   setEndpoints() {
-    this.emitter.on(this.path(METHOD.GET), this.getUser.bind(this))
-    this.emitter.on(this.path(METHOD.POST), this.createUser.bind(this))
-    this.emitter.on(this.path(METHOD.PUT), this.updateUser.bind(this))
-    this.emitter.on(this.path(METHOD.DELETE), this.removeUser.bind(this))
+    this.emitter.on(this.path(METHOD.GET), this.userService.getUser.bind(this))
+    this.emitter.on(this.path(METHOD.POST), this.userService.createUser.bind(this))
+    this.emitter.on(this.path(METHOD.PUT), this.userService.updateUser.bind(this))
+    this.emitter.on(this.path(METHOD.DELETE), this.userService.removeUser.bind(this))
   }
 
   listen(port: string | number, cb: () => void) {
@@ -164,7 +61,7 @@ export class Server {
       }
 
       const [root = '', path = '', parameter] = req.url!.split('/').slice(1)
-      const emit = this.emitter.emit(this.path(req.method!, root, path), req, res, parameter)
+      const emit = this.emitter.emit(this.path(req.method!, root, path), req, res, parameter, this.db)
       
       if (!emit) {
         res.writeHead(HttpCode.NotFound)
--- src/service/User.ts
+++ src/service/User.ts
@@ -1,21 +1,112 @@
-import { ReqType, ResType } from "../helpers/statusCode.js";
+import { v4, validate } from "uuid";
+import { checkCreate, checkUpdate } from "../helpers/checkReq.js";
+import { HttpCode, ReqType, ResType } from "../helpers/statusCode.js";
 import { IUser } from "../helpers/user.interface.js";
 
 export class UserService {
-  db: IUser[]
-
-  constructor(db: IUser[]) {
-    this.db = db
+  getUser(req: ReqType, res: ResType, parameter: string, db: IUser[]) {
+    if (parameter) {
+      const [user] = db.filter(el => el.id === parameter)
+      if (user) {
+        if (validate(user.id)) {
+          res.send(HttpCode.Success, user)
+        } else {
+          res.send(HttpCode.BadReq, { message: `this Id: ${user.id} is not valid` })
+        }
+      } else {
+        res.send(HttpCode.NotFound, { message: `there is no user with the same - ${parameter}` })
+      }
+    } else {
+      res.send(HttpCode.Success, db)
+    }
   }
-  update(req: ReqType, res: ResType) {
+
+  createUser(req: ReqType, res: ResType, parameter: string, db: IUser[]) {
+    if (parameter) {
+      res.send(HttpCode.NotFound, { message: 'Not Found' })
+      return
+    } else {
+      let body = ''
+      req.on('data', (chunk) => {
+        body += chunk
+      })
+
+      req.on('end', () => {
+        try {
+          const user = JSON.parse(body)
+          const errors = checkCreate(user)
+
+          if (errors.length === 0) {
+            const id = v4()
+            db.push({ id, username: user.username, age: user.age, hobbies: user.hobbies })
+            res.send(HttpCode.Created, { id, ...user })
+          } else {
+            res.send(HttpCode.BadReq, { errors })
+          }
+        } catch(e) {
+          res.send(HttpCode.ErrorServer, { message: `failed to create user` })
+        }
+      })
+    }
+  }
+
+  updateUser(req: ReqType, res: ResType, parameter: string, db: IUser[]) {
+    if (!parameter) {
+      res.send(HttpCode.NotFound, { message: `Not Found: id not specified` })
+      return
+    } else if (!validate(parameter)) {
+      res.send(HttpCode.NotFound, { message: `this Id: ${parameter} is not valid` })
+      return
+    }
+
+    let body = ''
+    req.on('data', (chunk) => {
+      body += chunk
+    })
+    req.on('end', () => {
+      try {
+        const user = JSON.parse(body)
+        const errors = checkUpdate(user)
+
+        if (errors.length === 0) {
+          const index = db.findIndex(el => el.id === parameter)
+          const prevItem = db[index] 
+          const userItem = {
+            id: user.id ? user.id : prevItem.id,
+            username: user.username ? user.username : prevItem.username,
+            age: user.age ? user.age : prevItem.age,
+            hobbies: user.hobbies ? user.hobbies : prevItem.hobbies,
+          }
+          
+          db.splice(index, 1, userItem)
+          
+          res.send(HttpCode.Success, userItem)
+
+        } else {
+          res.send(HttpCode.BadReq, { errors })
+        }
+      } catch(e) {
+        res.send(HttpCode.ErrorServer, { message: `failed to update user` })
+      }      
+    })
+  }
+
+  removeUser(req: ReqType, res: ResType, parameter: string, db: IUser[]) {
+    const userI = db.findIndex(el => el.id === parameter)
+    if (!parameter) {
+      res.send(HttpCode.BadReq, { message: `id not specified` })
+      return
+    } else if (!validate(parameter)) {
+      res.send(HttpCode.NotFound, { message: `this Id: ${parameter} is not valid` })
+      return
+    }
     
-  }  
-
-  delete(req: ReqType, res: ResType) {}
-
-  create(req: ReqType, res: ResType) {}
-
-  get(req: ReqType, res: ResType, db: IUser[]) {
-    res.send(db)
+    if (userI === -1) {
+      res.send(HttpCode.BadReq, { message: `there is no user with this id` })
+      return
+    }
+    
+    db.splice(userI, 1)
+    res.send(HttpCode.Delete , { message: `user with id: ${parameter} deleted` })
   }
 }
